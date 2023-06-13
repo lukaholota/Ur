ac7c4f70afd438cbece0ce65a7d1986c5a075295
Luka Holota <holotaluka@gmail.con>
2023-06-13 13:27:21 +0300

    alembic added

--- flask_app.py
+++ flask_app.py
@@ -1,34 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import dotenv_values, load_dotenv
+from dotenv import dotenv_values
 import redis
-import os
 
 env_vars = dotenv_values()
-POSTGRES_HOST = env_vars['POSTGRES_HOST']
-POSTGRES_PASSWORD = env_vars['POSTGRES_PASSWORD']
-POSTGRES_PORT = env_vars['POSTGRES_PORT']
-POSTGRES_USER = env_vars['POSTGRES_USER']
-POSTGRES_DB = env_vars['POSTGRES_DB']
-
-REDIS_BIND = env_vars['REDIS_BIND']
-REDIS_PORT = env_vars['REDIS_PORT']
-
-# load_dotenv()
-# POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-# POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-# POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-# POSTGRES_USER = os.getenv('POSTGRES_USER')
-# POSTGRES_DB = os.getenv('POSTGRES_DB')
-#
-# REDIS_BIND = os.getenv('REDIS_BIND')
-# REDIS_PORT = os.getenv('REDIS_PORT')
-
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
-app.config['REDIS_URL'] = f'redis://{REDIS_BIND}:{REDIS_PORT}'
+app.config['SQLALCHEMY_DATABASE_URI'] = env_vars['DB_URI']
+app.config['REDIS_URL'] = env_vars['RDB_URI']
 
 db = SQLAlchemy(app)
-rdb = redis.Redis.from_url(f'redis://{REDIS_BIND}:{REDIS_PORT}')
+rdb = redis.Redis.from_url(env_vars['RDB_URI'])
